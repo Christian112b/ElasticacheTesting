cb347ca1044a029a8ea28ba7dd21144476d0a794
Christian Zavala <crizaaron@gmail.com>
2024-11-30 10:04:59 -0600

    bug

--- functions/getElasticacheInfo.py
+++ functions/getElasticacheInfo.py
@@ -3,6 +3,7 @@ from controllers.cacheController import Redis
 def getElasticacheInfo():
     redis_instance = Redis()
 
-    keys = redis_instance.scanKeys(redis_instance)
-
-    print(keys)
+    #keys = redis_instance.scanKeys(redis_instance)
+    for key in redis_instance.scan_iter("user:*"):
+        print(key)
+        #r.delete(key)
