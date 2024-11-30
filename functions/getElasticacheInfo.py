b28fd75707691cd2dd90ba5e0aebff12f67d8284
Christian Zavala <crizaaron@gmail.com>
2024-11-30 10:12:15 -0600

    bug

--- functions/getElasticacheInfo.py
+++ functions/getElasticacheInfo.py
@@ -7,6 +7,6 @@ def getElasticacheInfo():
     for i in range(1000):
         key = f"user:{random.randint(1000, 9999)}"  # Genera una clave aleatoria
         value = f"data:{random.randint(1000, 9999)}"  # Genera un valor aleatorio
-        redis_instance.set(key, value)  # Ejecuta SET
+        redis_instance.redis_client.set(key, value)  # Ejecuta SET
         time.sleep(0.01)  # Pausa de 10 ms entre cada operación para no sobrecargar el clúster
         
