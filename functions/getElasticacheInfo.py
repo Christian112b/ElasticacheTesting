f4722b72a551d314f4f31a5decd646727a20a550
Christian Zavala <crizaaron@gmail.com>
2024-11-30 10:13:21 -0600

    bug

--- functions/getElasticacheInfo.py
+++ functions/getElasticacheInfo.py
@@ -4,9 +4,10 @@ from controllers.cacheController import Redis
 
 def getElasticacheInfo():
     redis_instance = Redis()
-    for i in range(1000):
+    for i in range(100000):
         key = f"user:{random.randint(1000, 9999)}"  # Genera una clave aleatoria
         value = f"data:{random.randint(1000, 9999)}"  # Genera un valor aleatorio
+        print(value)
         redis_instance.redis_client.set(key, value)  # Ejecuta SET
         time.sleep(0.01)  # Pausa de 10 ms entre cada operación para no sobrecargar el clúster
         
